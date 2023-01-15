4b3de584b9a349b8a56f16f3632b9c0052ff6e14
Gustavo Rondello <gustavorondello99@outlook.com>
2023-01-14 21:17:02 -0300

    test: teste "test_product.py" implementado

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,24 @@
-# from inventory_report.inventory.product import Product
-# primeiro commit
+from inventory_report.inventory.product import Product
+
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    """Testa criar um novo produto com atributos corretamente preenchidos"""
+    mocked_product = Product(
+        "01",
+        "Suco de Pera",
+        "SehLoiro inc",
+        "18/04/2015",
+        "10/03/2021",
+        "FON01",
+        "Manter em lugar gelado",
+    )
+
+    assert mocked_product.id == "01"
+    assert mocked_product.nome_do_produto == "Suco de Pera"
+    assert mocked_product.nome_da_empresa == "SehLoiro inc"
+    assert mocked_product.data_de_fabricacao == "18/04/2015"
+    assert mocked_product.data_de_validade == "10/03/2021"
+    assert mocked_product.numero_de_serie == "FON01"
+    assert (
+        mocked_product.instrucoes_de_armazenamento == "Manter em lugar gelado"
+    )
